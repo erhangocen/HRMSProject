5089546114ecc05639588f598591e2154304b201
karteleros <71042486+karteleros@users.noreply.github.com>
2021-05-13 20:49:36 +0300

    .

--- src/main/java/kodlamaio/humanResourcesProject/api/JobPositionsController.java
+++ src/main/java/kodlamaio/humanResourcesProject/api/JobPositionsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/jobpositions")
+@RequestMapping("/api/jobpositions/")
 public class JobPositionsController {
 
     private final IJobPositionService _jobPositionService;
@@ -20,7 +20,7 @@ public class JobPositionsController {
         this._jobPositionService = _jobPositionService;
     }
 
-    @GetMapping("/getall")
+    @GetMapping("getall")
     public List<JobPosition> getAll(){
         return _jobPositionService.getAll();
     }
